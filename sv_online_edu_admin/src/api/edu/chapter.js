13404f9665fe75a1d6bc4b663db048c8276b37eb
owen-zhou20 <78416546+owen-zhou20@users.noreply.github.com>
2022-08-25 17:00:19 +1000

    Add add, modify, delete chapter information in add course function.
    Add add, modify, delete chapter information in add course function.

--- sv_online_edu_admin/src/api/edu/chapter.js
+++ sv_online_edu_admin/src/api/edu/chapter.js
@@ -7,5 +7,35 @@ export default {
       url: `/eduservice/chapter/getChapterVideo/` + courseId,
       method: 'get'
     })
+  },
+  // Get all course chapters list include all videos list by course id
+  addChapter(chapter) {
+    return request({
+      url: `/eduservice/chapter/addChapter`,
+      method: 'post',
+      data: chapter
+    })
+  },
+  // Get chapter info by id
+  getChapter(chapterId) {
+    return request({
+      url: `/eduservice/chapter/getChapterInfo/` + chapterId,
+      method: 'get'
+    })
+  },
+  // Modify chapter info by id
+  updateChapter(chapter) {
+    return request({
+      url: `/eduservice/chapter/updateChapter`,
+      method: 'post',
+      data: chapter
+    })
+  },
+  // Delete chapter info by id
+  deleteChapter(chapterId) {
+    return request({
+      url: `/eduservice/chapter/` + chapterId,
+      method: 'delete'
+    })
   }
 }
--- sv_online_edu_admin/src/views/edu/course/chapter.vue
+++ sv_online_edu_admin/src/views/edu/course/chapter.vue
@@ -6,11 +6,50 @@
               align-center
               style="margin-bottom: 40px;">
       <el-step title="Fill in this course infomation" />
-      <el-step title="Create course outline" />
+      <el-step title="Create course outline (chapters and sections)" />
       <el-step title="Release new course" />
     </el-steps>
 
+    <el-button type="text"
+               @click="openChapterDialog()">Add chapter</el-button>
+
     <!-- Chapter -->
+    <ul class="chanpterList">
+      <li v-for="chapter in chapterVideoList"
+          :key="chapter.id">
+        <p>
+          {{ chapter.title }}
+          <span class="acts">
+            <el-button type="text">Add section</el-button>
+            <el-button style=""
+                       type="text"
+                       @click="openEditChapter(chapter.id)">Modify</el-button>
+            <el-button type="text"
+                       @click="removeChapter(chapter.id)">Delete</el-button>
+          </span>
+        </p>
+        <!-- Section (Video) -->
+        <ul class="chanpterList videoList">
+          <li v-for="video in chapter.children"
+              :key="video.id">
+            <p>{{ video.title }}
+              <span class="acts">
+                <el-button type="text">Modify</el-button>
+                <el-button type="text">Delete</el-button>
+              </span>
+            </p>
+          </li>
+        </ul>
+      </li>
+    </ul>
+    <div>
+      <el-button @click="previous">Previous</el-button>
+      <el-button :disabled="saveBtnDisabled"
+                 type="primary"
+                 @click="next">Save and next</el-button>
+    </div>
+
+    <!-- Chapter
     <ul class="chanpterList">
       <li v-for="chapter in chapterVideoList"
           :key="chapter.id">
@@ -23,7 +62,7 @@
             <el-button type="text">删除</el-button>
           </span>
         </p>
-        <!-- 视频 -->
+        <!-- 视频
         <ul class="chanpterList videoList">
           <li v-for="video in chapter.children"
               :key="video.id">
@@ -46,6 +85,29 @@
                    @click="next">Save and next</el-button>
       </el-form-item>
     </el-form>
+    -->
+
+    <!-- Add and modify chapter dialog -->
+    <el-dialog :visible.sync="dialogChapterFormVisible"
+               title="Add chapter">
+      <el-form :model="chapter"
+               label-width="120px">
+        <el-form-item label="Chapter title">
+          <el-input v-model="chapter.title" />
+        </el-form-item>
+        <el-form-item label="Chapter sort">
+          <el-input-number v-model="chapter.sort"
+                           :min="0"
+                           controls-position="right" />
+        </el-form-item>
+      </el-form>
+      <div slot="footer"
+           class="dialog-footer">
+        <el-button @click="dialogChapterFormVisible = false">Cancel</el-button>
+        <el-button type="primary"
+                   @click="saveOrUpdate">Save</el-button>
+      </div>
+    </el-dialog>
   </div>
 </template>
 
@@ -57,7 +119,12 @@ export default {
     return {
       saveBtnDisabled: false,
       courseId: '', // course id
-      chapterVideoList: []
+      chapterVideoList: [],
+      chapter: { // Chapter data
+        title: '',
+        sort: 0
+      },
+      dialogChapterFormVisible: false // Show dialog chapter
     }
   },
   created() {
@@ -68,6 +135,85 @@ export default {
     }
   },
   methods: {
+    // Delete chapter
+    removeChapter(chapterId) {
+      this.$confirm('This will permanently delete this chapter record. Continue?', 'Warning', {
+        confirmButtonText: 'OK',
+        cancelButtonText: 'Cancel',
+        type: 'warning'
+      }).then(() => {
+        // call delete chapter method
+        chapter.deleteChapter(chapterId)
+          .then(response => { // success to delete chapter by id
+            this.$message({
+              type: 'success',
+              message: 'Delete completed'
+            })
+            // Refresh
+            this.getChapterVideo()
+          })
+        // notice message
+      }).catch(() => {
+        this.$message({
+          type: 'info',
+          message: 'Delete canceled'
+        })
+      })
+    },
+    // Modify chapter
+    openEditChapter(chapterId) {
+      this.dialogChapterFormVisible = true
+      chapter.getChapter(chapterId)
+        .then(response => {
+          this.chapter = response.data.chapter
+        })
+    },
+    // Add chapter dialog
+    openChapterDialog() {
+      this.dialogChapterFormVisible = true
+      this.chapter.title = ''
+      this.chapter.sort = 0
+    },
+    // Add chapter
+    addChapter() {
+      // Set course id into chapter
+      this.chapter.courseId = this.courseId
+      chapter.addChapter(this.chapter)
+        .then(response => {
+          // Close dialog
+          this.dialogChapterFormVisible = false
+          // Notice
+          this.$message({
+            type: 'success',
+            message: 'Success to add this chapter!'
+          })
+          // Refresh
+          this.getChapterVideo()
+        })
+    },
+    // Modify chapter
+    updateChapter() {
+      chapter.updateChapter(this.chapter)
+        .then(response => {
+          // Close dialog
+          this.chapter.id = ''
+          this.dialogChapterFormVisible = false
+          // Notice
+          this.$message({
+            type: 'success',
+            message: 'Success to modify this chapter!'
+          })
+          // Refresh
+          this.getChapterVideo()
+        })
+    },
+    saveOrUpdate() {
+      if (!this.chapter.id) {
+        this.addChapter()
+      } else {
+        this.updateChapter()
+      }
+    },
     // Get all course chapters list include all videos list by course id
     getChapterVideo() {
       chapter.getAllChapterVideo(this.courseId)
@@ -115,7 +261,7 @@ export default {
   padding-left: 50px;
 }
 .videoList p {
-  float: left;
+  /*float: left;*/
   font-size: 14px;
   margin: 10px 0;
   padding: 10px;
