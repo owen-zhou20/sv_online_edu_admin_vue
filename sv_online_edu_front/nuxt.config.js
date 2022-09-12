aa6118518803d91446e0ab40e052f5d03e8a8dac
owen-zhou20 <78416546+owen-zhou20@users.noreply.github.com>
2022-09-12 18:05:14 +1000

    Create a new project sv_online_edu_front as the website for customers in the sv_online_edu_vue system. Set up the NUXT environment for this project.
    Create a new project sv_online_edu_front as the website for customers in the sv_online_edu_vue system. Set up the NUXT environment for this project.

---
+++ sv_online_edu_front/nuxt.config.js
@@ -0,0 +1,46 @@
+module.exports = {
+  plugins: [
+    { src: '~/plugins/nuxt-swiper-plugin.js', ssr: false }
+  ],
+  css: [
+    'swiper/dist/css/swiper.css'
+  ],
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: 'SV online education website - Learn Java, JavaScript and Vue etc. videos',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'keywords', name: 'keywords', content: 'SV online education website. Free IT online videos website- Learn Java, JavaScript and Vue etc. videos' },
+      { hid: 'description', name: 'description', content: 'SV online education website. Free to learn IT online videos - Learn Java, JavaScript and Vue etc. videos' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+  /*
+  ** Customize the progress bar color
+  */
+  loading: { color: '#3B8070' },
+  /*
+  ** Build configuration
+  */
+  build: {
+    /*
+    ** Run ESLint on save
+    */
+    extend(config, { isDev, isClient }) {
+      if (isDev && isClient) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/
+        })
+      }
+    }
+  }
+}
+
